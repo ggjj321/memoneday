421084656f3339fd3154ab470890e2e1dd24b532
ggjj321 <10370324lsjh@gmail.com>
2021-09-21 13:02:07 +0800

    firestore ok will rebuild

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 const AndroidNotificationChannel channel = AndroidNotificationChannel(
     'high_importance_channel', // id
@@ -78,6 +79,35 @@ class MyStatefulWidget extends StatefulWidget {
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
+class GetUserName extends StatelessWidget {
+  final int documentId;
+
+  GetUserName(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance
+          .collection('users')
+          .where('id', isEqualTo: this.documentId)
+          .get(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          var docs = snapshot.data!.docs;
+          print(docs.length);
+          return Text("Full Name: ${docs[0]['message']}");
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   final myController = TextEditingController();
   int _counter = 0;
@@ -107,9 +137,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   void initState() {
     super.initState();
-    setState(() {
-      _asyncMethod();
-    });
+    // setState(() {
+    //   _asyncMethod();
+    // });
     // Start listening to changes.
     myController.addListener(_printLatestValue);
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
@@ -161,8 +191,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     int hour = now.hour, minute = now.minute;
     int selechour = int.parse(_selectTimelist[0]),
         seleminute = int.parse(_selectTimelist[1]);
-    if(_selectedlist[1] == 'PM' && selechour != 12) selechour+=12;
-    if(_selectedlist[1] == 'AM' && selechour == 12) selechour=0; //24-hour clock
+    if (_selectedlist[1] == 'PM' && selechour != 12) selechour += 12;
+    if (_selectedlist[1] == 'AM' && selechour == 12)
+      selechour = 0; //24-hour clock
 
     if (minute > seleminute) {
       seleminute += 60;
@@ -171,7 +202,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
     minute = seleminute - minute;
 
-    if (hour > selechour)// 隔天
+    if (hour > selechour) // 隔天
       hour = 24 - (hour - selechour);
     else
       hour = selechour - hour;
@@ -206,105 +237,172 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     print('Second text field: ${myController.text}');
   }
 
+  String? _readMessage(idx) {
+    dynamic text = '';
+    FirebaseFirestore.instance
+        .collection('users')
+        .where('id', isEqualTo: idx)
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        text = doc["message"];
+      });
+    });
+    return text;
+  }
+
   @override
   Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
     return Scaffold(
       appBar: AppBar(
         title: Text('備忘錄'),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: 2 / 3,
-        children: List.generate(taskNum, (idx) {
-          return Card(
-            elevation: 10.0,
-            color: Colors.tealAccent,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20.0))),
-            child: Container(
-                alignment: Alignment.topCenter,
-                width: 100,
-                height: 100,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: 150,
-                      height: 60,
-                      child: TextField(
-                        controller: new TextEditingController(),
-                        textInputAction: TextInputAction.newline,
-                        keyboardType: TextInputType.multiline,
-                        textAlign: TextAlign.start,
-                        onChanged: (text) async {
-                          var task = Task(
-                            id: idx,
-                            task: text,
-                          );
-                          await TaskDB.insertData(task);
-                          // wardList[idx] = text;
-                          // print(text);
-                        },
-                        decoration: new InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          suffix: IconButton(
-                              icon: Icon(Icons.add),
-                              onPressed: () async {
-                                // print(await TaskDB.getonedata(idx));
-                                // print(await TaskDB.getCount());
-                                wardList[idx] = await TaskDB.getonedata(idx);
-                                setState(() {});
-                              }),
-                        ),
-                      ),
-                    ),
-                    Text(
-                      '${wardList[idx]}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                      ),
-                    ),
-                    Expanded(
-                      child: Row(
+      body: StreamBuilder(
+          stream: users.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: Text('Loading'));
+            }
+            return GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: 2 / 3,
+              children: List.generate(taskNum, (idx) {
+                final thiscontroller = new TextEditingController();
+
+                // var thistext =  _readMessage(idx);
+                // print('init: $thistext');
+
+                return Card(
+                  elevation: 10.0,
+                  color: Colors.tealAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  child: Container(
+                      alignment: Alignment.topCenter,
+                      width: 100,
+                      height: 100,
+                      child: Column(
                         children: [
-                          Container(
-                            alignment: Alignment.bottomRight,
-                            child: IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed: () async {
-                                await TaskDB.deleteData(idx);
-                                taskNum -= 1;
-                                wardList.removeAt(idx);
-                                setState(() {});
-                              },
+                          SizedBox(
+                            width: 150,
+                            height: 60,
+                            child: TextField(
+                              controller: thiscontroller,
+                              textInputAction: TextInputAction.newline,
+                              keyboardType: TextInputType.multiline,
+                              textAlign: TextAlign.start,
+                              decoration: new InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                suffix: IconButton(
+                                    icon: Icon(Icons.add),
+                                    onPressed: () async {
+                                      users
+                                          .doc('message$idx')
+                                          .set(
+                                            {
+                                              'message': thiscontroller.text,
+                                              'id': idx,
+                                            },
+                                            SetOptions(merge: true),
+                                          )
+                                          .then((value) => print(
+                                              "'full_name' & 'age' merged with existing data!"))
+                                          .catchError((error) => print(
+                                              "Failed to merge data: $error"));
+                                      // thistext = _readMessage(idx);
+                                      // print(idx);
+                                      // print(thistext);
+                                      //print(thiscontroller.text);
+                                      // FirebaseFirestore.instance
+                                      //     .collection('users')
+                                      //     .where('id', isEqualTo: idx)
+                                      //     .get()
+                                      //     .then((QuerySnapshot querySnapshot) {
+                                      //   querySnapshot.docs.forEach((doc) {
+                                      //     print('**');
+                                      //     print(doc["message"]);
+                                      //   });
+                                      // });
+                                      setState(() {});
+                                    }),
+                              ),
                             ),
                           ),
-                          Container(
-                            alignment: Alignment.bottomCenter,
-                            child: IconButton(
-                              icon: Icon(Icons.send),
-                              onPressed:(){
-                                showNotification(wardList[idx], idx);
+                          FutureBuilder<QuerySnapshot>(
+                            future: FirebaseFirestore.instance
+                                .collection('users')
+                                .where('id', isEqualTo: idx)
+                                .get(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<QuerySnapshot> snapshot) {
+                              if (snapshot.hasError) {
+                                return Text("Something went wrong");
                               }
-                            ),
+
+                              if (snapshot.connectionState ==
+                                  ConnectionState.done) {
+                                var docs = snapshot.data!.docs;
+                                return Text("${docs[0]['message']}");
+                              }
+
+                              return Text("loading");
+                            },
                           ),
-                          Container(
-                            alignment: Alignment.bottomLeft,
-                            child: IconButton(
-                              icon: Icon(Icons.lock_clock),
-                              onPressed: _showtime,
+                          // Text(
+                          //   // '${FirebaseFirestore.instance
+                          //   //     .collection('users')
+                          //   //     .where('age', isGreaterThan: 20)
+                          //   //     .get()
+                          //   // }'
+                          //   '$thistext',
+                          //   style: TextStyle(
+                          //     fontWeight: FontWeight.bold,
+                          //     fontSize: 40,
+                          //   ),
+                          // ),
+                          Expanded(
+                            child: Row(
+                              children: [
+                                Container(
+                                  alignment: Alignment.bottomRight,
+                                  child: IconButton(
+                                    icon: Icon(Icons.delete),
+                                    onPressed: () async {
+                                      await TaskDB.deleteData(idx);
+                                      taskNum -= 1;
+                                      wardList.removeAt(idx);
+                                      setState(() {});
+                                    },
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.bottomCenter,
+                                  child: IconButton(
+                                      icon: Icon(Icons.send),
+                                      onPressed: () {
+                                        showNotification(wardList[idx], idx);
+                                      }),
+                                ),
+                                Container(
+                                  alignment: Alignment.bottomLeft,
+                                  child: IconButton(
+                                    icon: Icon(Icons.lock_clock),
+                                    onPressed: _showtime,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
-                      ),
-                    ),
-                  ],
-                )),
-          );
-        }),
-      ),
+                      )),
+                );
+              }),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
